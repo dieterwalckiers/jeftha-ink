29cc01066810d7c72be182a29ae25eba43291a35
dyte <dieter.walckiers@gmail.com>
2020-03-01 22:27:09 +0100

    hover effect

--- src/components/gallery/ProjectTile.js
+++ src/components/gallery/ProjectTile.js
@@ -1,12 +1,10 @@
-import React from "react"
+import React, { useState, useRef } from "react"
 import Image from "gatsby-image"
 
 const Header = ({ project }) => {
   return (
     <div className="md:hidden">
-      <p className="text-3xl m-0 font-thin">
-        {project.title}
-      </p>
+      <p className="text-3xl m-0 font-thin">{project.title}</p>
       <p className="my-2 text-sm m-0">{project.description}</p>
     </div>
   )
@@ -14,10 +12,56 @@ const Header = ({ project }) => {
 
 const ProjectTile = props => {
   const { project, highlightsStyle } = props
+  const wrapperRef = useRef(null)
+
+  const [wrapperStyle, setWrapperStyle] = useState({})
+  const [imageStyle, setImageStyle] = useState({})
+  const [origDims, setOrigDims] = useState()
+
+  const startHover = () => {
+    const {
+      clientWidth: currentWidth,
+      clientHeight: currentHeight,
+    } = wrapperRef.current
+    setOrigDims([currentWidth, currentHeight])
+    const width = currentWidth * 1.05
+    const height = currentHeight * 1.05
+    setWrapperStyle({ width: currentWidth, height: currentHeight })
+    setImageStyle({
+      ...imageStyle,
+      top: 0,
+      left: 0,
+      width: currentWidth,
+      height: currentHeight,
+    })
+    setTimeout(() => {
+      setImageStyle({ ...imageStyle, width, height, top: -10, left: -10 })
+    }, 0)
+  }
+
+  const stopHover = () => {
+    origDims &&
+      setImageStyle({ ...imageStyle, width: origDims[0], height: origDims[1], top: 0, left: 0 })
+  }
+
   return (
-    <div className="ml-2 mr-2 m-2 md:mb-4">
+    <div
+      onMouseEnter={startHover}
+      onMouseLeave={stopHover}
+      ref={wrapperRef}
+      style={wrapperStyle}
+      className="m-2 md:mb-4 overflow-hidden relative"
+    >
       {!highlightsStyle && <Header project={project} />}
-      <Image fluid={project.mainImage.asset.fluid} alt={project.title} />
+      <Image
+        fluid={project.mainImage.asset.fluid}
+        alt={project.title}
+        className="absolute"
+        style={{
+          ...imageStyle,
+          transition: "width 1s, height 1s, top 1s, left 1s",
+        }}
+      />
     </div>
   )
 }
