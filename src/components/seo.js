56df0c4a01d561dcc88fe9a90283919abd8a35cd
dieter <dieter@manual.to>
2020-07-08 11:47:22 +0200

    meta description

--- src/components/seo.js
+++ src/components/seo.js
@@ -37,7 +37,7 @@ function SEO({ description, lang, meta, title }) {
       meta={[
         {
           name: `description`,
-          content: metaDescription,
+          content: "Jeftha.ink artiestenpagina", // metaDescription,
         },
         {
           property: `og:title`,
